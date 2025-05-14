edcc1c335f0243369ddbe25fb2f2f63a50772c7c
Henrique <naldotbr2000@gmail.com>
2025-05-14 12:40:59 -0300

    refeat - editar turmas ja criadas

--- lib/src/view/home/crud_turma/crud_turma_view_model.dart
+++ lib/src/view/home/crud_turma/crud_turma_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gestao_de_notas/src/domain/domain.dart';
 import 'package:gestao_de_notas/src/models/aluno_model.dart';
@@ -28,6 +30,7 @@ class CrudTurmaViewModel {
     List<TurmaModel> lista = [];
     lista.addAll(await domain.getTurmas());
     TurmaModel turma = TurmaModel(
+      id: await createIdTurma(),
       nome: nomeDaTurma.value.text,
       escola: escolaSelecionada.value,
       ano: anoSelecionado.value,
@@ -124,5 +127,57 @@ class CrudTurmaViewModel {
     if (newValue != null) {
       nomeDaTurma.value.text = newValue;
     }
+  }
+
+  ValueNotifier<bool> checkEditor = ValueNotifier(false);
+
+  void editor(TurmaModel turmaParaEditar) async {
+    checkEditor.value = true;
+    anoSelecionado.value = turmaParaEditar.ano;
+    escolaSelecionada.value = turmaParaEditar.escola;
+    disciplinaSelecionado.value = turmaParaEditar.disciplina;
+    nomeDaTurma.value.text = turmaParaEditar.nome;
+    listaDeAlunos.value = turmaParaEditar.alunos;
+    await Future.delayed(Duration(microseconds: 1));
+    checkEditor.value = false;
+  }
+
+  Future<void> editarTurma(String id) async {
+    isLoading.value = true;
+    List<TurmaModel> lista = [];
+    lista.addAll(await domain.getTurmas());
+
+    TurmaModel turma = TurmaModel(
+      id: id,
+      nome: nomeDaTurma.value.text,
+      escola: escolaSelecionada.value,
+      ano: anoSelecionado.value,
+      disciplina: disciplinaSelecionado.value,
+      alunos: listaDeAlunos.value,
+    );
+
+    lista[lista.indexWhere((turmaIndex) => turma.id == turmaIndex.id)] = turma;
+
+    listaDeTurmas.value = lista;
+    await domain.setTurmas(lista);
+    isLoading.value = false;
+  }
+
+  Future<String> createIdTurma() async {
+    final chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    final random = Random();
+    List<String> idsGerados =
+        (await domain.getTurmas()).map((e) => e.id).toList();
+
+    String novoId;
+
+    do {
+      novoId =
+          List.generate(
+            8,
+            (index) => chars[random.nextInt(chars.length)],
+          ).join();
+    } while (idsGerados.contains(novoId));
+    return novoId;
   }
 }
